5ee59f93faf0c16c8a9b038a860e30fce2da9435
Dan Racanelli <accounts@attnspan.com>
2012-07-22 17:07:04 -0700

    committing before further segmenting data

--- localityFinder.js
+++ localityFinder.js
@@ -58,27 +58,33 @@ var localityFinder = (function () {
         
         var localityObj,
             idx = data.length - 1,
-            cacheInfo = {},
+            cacheInfo = null,
             bounds = [],
             result = {},
+            foundData = false,
             dataIdx;
         while (idx--) {
             localityObj = data[idx];
-            if (localityObj.types && localityObj.types.indexOf('locality') !== -1) {
+            if (localityObj.types && localityObj.types.indexOf(field || 'locality') !== -1) {
+                foundData = true;
                 break;
             }
         }
 
-        if (localityObj.formatted_address) {
-            cacheInfo.name = localityObj.formatted_address.split(',').slice(0,2).join(',');
-        }
-        if (localityObj.geometry && (bounds = localityObj.geometry.bounds)) {
-            cacheInfo.bounds = { 
-                lat : [ bounds.southwest.lat, bounds.northeast.lat ],
-                lng : [ bounds.southwest.lng, bounds.northeast.lng ]
-            };
+        if (foundData) {
+            cacheInfo = {};
+            if (localityObj.formatted_address) {
+                cacheInfo.name = localityObj.formatted_address.split(',').slice(0,2).join(',');
+            }
+            if (localityObj.geometry && (bounds = localityObj.geometry.bounds)) {
+                cacheInfo.bounds = { 
+                    lat : [ bounds.southwest.lat, bounds.northeast.lat ],
+                    lng : [ bounds.southwest.lng, bounds.northeast.lng ]
+                };
+            }
         }
 
+        console.log('DATA?', cacheInfo);
         return cacheInfo;
     };
 
@@ -111,8 +117,6 @@ var localityFinder = (function () {
             len  = data.length,
             fd   = fs.openSync('./revgeo_data.json', 'w+');
 
-        // Read existing data
-
         // Write back updated data
         fd && fs.writeSync(fd, data, 0, len, null);
         fs.closeSync(fd);
@@ -133,12 +137,23 @@ var localityFinder = (function () {
                 result += chunk;
             });
             res.on('end', function(e) {
-                //console.log('end', result);
                 var data    = JSON.parse(result),
                     results = data.results;
                 
                 if (results && results.length) {
+                    // Cache data from google temporarily for debugging
+                    var data = JSON.stringify(results),
+                        len  = data.length,
+                        fd   = fs.openSync('./data.tmp.json', 'w+');
+
+                    // Write back updated data
+                    fd && fs.writeSync(fd, data, 0, len, null);
+                    fs.closeSync(fd);
+
                     cacheInfo = parseGoogleData(results);        
+                    if (!cacheInfo) {
+                        cacheInfo = parseGoogleData(results, 'administrative_area_level_2');        
+                    }
                     cb && cacheInfo && cb(cacheInfo);
                 }
             });
@@ -157,50 +172,43 @@ var localityFinder = (function () {
 
 })();
 
-var fs   = require('fs'),
-    http = require('http');
+var fs       = require('fs'),
+    http     = require('http'),
+    start_ts = +new Date;
 
 fs.readFile('./revgeo_data.json', 'utf8', function (err, data) {
-    var parsed_data = JSON.parse(data),
+    var got_data_ts = +new Date,
+        got_data_in = (got_data_ts - start_ts),
+        parsed_data = JSON.parse(data),
         results     = parsed_data && parsed_data.results,
         results_len = results && results.length;
 
     localityFinder.setLocalityData(parsed_data);
 
-    var lng, lat, locality;
-    if (process.argv.length === 4) {
-        lng = process.argv.pop();
-        lat = process.argv.pop();
-    
+    var latlng, lng, lat, locality, now;
+    if (process.argv.length > 2) {
+        switch (process.argv.length) {
+            case 3:
+                latlng = process.argv.pop().split(' ');
+                break;
+            case 4:
+                latlng = process.argv;
+                break;
+        }
+
+        lng = latlng.pop();
+        lat = latlng.pop();
+        
         locality = localityFinder.getLocalityInfo([lat,lng]);
         if (locality) {
-            console.log("data from cache", locality);
+            now = +new Date;
+            console.log("data from cache (%dms)", (now - got_data_ts), locality);
         } else {
             var info = localityFinder.getLocalityDataFromGoogle([lat,lng], function(info) {
-                console.log('zomg info', info);
+                now = +new Date;
+                console.log("data from google (%dms)", (now - got_data_ts), info);
                 info && localityFinder.cacheData(info);
             });
         }
     }
 });
-
-//fs.readFile('./rome_data.json', 'utf8', function (err, data) {
-//    var parsed_data = JSON.parse(data),
-//        results     = parsed_data && parsed_data.results,
-//        results_len = results && results.length;
-//
-//    var info = localityFinder.parseGoogleData(results);
-//
-//    localityFinder.cacheData(info);
-//});
-
-//var lng, lat;
-//if (process.argv.length === 4) {
-//    lng = process.argv.pop();
-//    lat = process.argv.pop();
-//
-//    //if ( localityFinder.isBetweenCoords(lat, cacheInfo.bounds.lat) 
-//    //     && localityFinder.isBetweenCoords(lng, cacheInfo.bounds.lng)) {
-//    //    console.log("You are in " + cacheInfo.name);
-//    //}
-//}
